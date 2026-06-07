a60b7d7ccf122eabb1df22be710004fc60949add
inevitablehawk29 <191446718+inevitablehawk29@users.noreply.github.com>
2026-06-07 18:38:12 +0530

    Optimize image breakpoints by adding custom deviceSizes

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,19 @@ const nextConfig: NextConfig = {
   images: {
     formats: ["image/avif", "image/webp"],
     qualities: [75, 85],
+    deviceSizes: [
+      640,
+      750,
+      828,
+      1080,
+      1200,
+      1280,
+      1440,
+      1600,
+      1920,
+      2048,
+      3840,
+    ],
   },
   async headers() {
     return [
